77c250b1454a0959130117a7f6bc1437d42d2889
Bijay <bijayrana13@gmail.com>
2020-04-17 05:31:37 -0700

    ColorAppCommit Commit

--- src/main/java/com/hub/bijay/kafka/streams/ColorCodeApp.java
+++ src/main/java/com/hub/bijay/kafka/streams/ColorCodeApp.java
@@ -3,6 +3,7 @@ package com.hub.bijay.kafka.streams;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
@@ -16,25 +17,35 @@ public class ColorCodeApp {
     public static void main(String[] args) {
 
         Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG,"Wordcount-Application");
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG,"ColorCount-Application");
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
 
         KStreamBuilder builder = new KStreamBuilder();
 
-        KStream<String, String> WordCountInput = builder.stream("word-count-input");
-        KTable<String, Long> WordCounts = WordCountInput.mapValues(value -> value.toLowerCase())
-                .flatMapValues(value -> Arrays.asList(value.split(" ")))
-                .selectKey((key,value) -> value)
-                .groupByKey()
-                .count("Counts");
+        KStream<String, String> ColorCountInput = builder.stream("color-count-input");
 
-        WordCounts.to(Serdes.String(), Serdes.Long(),"word-count-output");
+        KStream<String, String> ColorCounts = ColorCountInput.filter((k,v) -> v.contains(","))
+                .selectKey((k, v) -> v.split(",")[0].toLowerCase())
+                .mapValues(value -> value.split(",")[1].toLowerCase())
+                .filter((key, value) -> Arrays.asList("red","green","blue").contains(value));
+
+
+        ColorCounts.to(Serdes.String(), Serdes.String(),"color-count-intermediate");
+
+        KTable<String, String> colorTable = builder.table("color-count-intermediate");
+
+        KTable<String, Long> CountStreamFinalTable = colorTable.groupBy((k,v) -> new KeyValue<>(v,v)).count("COUNTS");
+
+        CountStreamFinalTable.to(Serdes.String(),Serdes.Long(),"color-count-output");
+
 
         KafkaStreams streams = new KafkaStreams(builder, config);
 
+        streams.cleanUp();
         streams.start();
         //Printing topology
         System.out.println(streams.toString());
